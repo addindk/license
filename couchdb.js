a0d2c4d1ff5b9eba5057ccd8ae72980ef9eb8b02
Rune Tvilum <rune@addin.dk>
2016-02-15 14:26:45 +0100

    test

--- couchdb.js
+++ couchdb.js
@@ -33,30 +33,23 @@ app.put('/:id', function (req, res) {
         req.body += chunk;
     });
     req.on('end', function () {
-        
+
         console.log(req.headers);
         console.log(req.params.id);
         console.log(req.body);
-        req.body =JSON.parse(req.body);
+        req.body = JSON.parse(req.body);
         console.log(req.body);
         new Promise(function (resolve, reject) {
             var user = basicAuth(req);
-            if (user) {
-                db.one(sqlProvider.authorize, user).then(function (res) {
-                    console.log('1',res);
-                    if (res.test) {
-                        resolve({ product: req.params.id, customer: res.customer });
-                    } else {
-                        reject();
-                    }
-                });
+            if (user && req.body.product) {
+                resolve({ customer: user.name, product: req.body.product });
             } else {
                 reject();
             }
         }).then(function (data) {
             return db.any(sqlProvider.product, data);
         }).then(function (data) {
-            console.log('2',data);
+            console.log('2', data);
             if (data.length === 1) {
                 var status = 1;
                 if (req.body.message && req.body.message === 'Stop') {
@@ -65,11 +58,11 @@ app.put('/:id', function (req, res) {
                 var doc = {
                     id: uuid.v1(),
                     login: req.body.login,
-                    machine: req.body.mashine,
+                    machine: req.body.machine,
                     status: status,
-                    product_id: data[0].product,
+                    product_id: req.body.product,
                     product_version: req.body.version,
-                    customer_id: data[0].customer,
+                    customer_id: req.params.id,
                     log_timestamp: new Date(),
                     ip: req.ip
                 }
@@ -79,10 +72,10 @@ app.put('/:id', function (req, res) {
                 return Promise.reject();
             }
         }).then(function (res) {
-            console.log('3',res);
+            console.log('3', res);
             res.send('ok');
         }).catch(function (err) {
-            console.log('err',err);
+            console.log('err', err);
             res.set('WWW-Authenticate', 'Basic realm=Authorization Required');
             res.send(401);
         });
